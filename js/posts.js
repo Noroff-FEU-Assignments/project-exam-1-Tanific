9d6bdf46fd554fa0ca650467608169333d278257
Tonje Stensen <stensentonje@gmail.com>
2023-01-08 08:53:58 +0100

    wow

--- js/posts.js
+++ js/posts.js
@@ -12,9 +12,14 @@ async function getPosts() {
     for(let i = 0; i < json.length; i++) {
 
         container.innerHTML += `
-        <a href="post-specific.html?id=${json[i].id}"<div class=post"><h2>${json[i].title.rendered}</h2><p>${json[i].excerpt.rendered}
-        <img src="${json[i]._embedded["wp:featuredmedia"]["0"].source_url}/>
-        </div></a>`
+        <a href="post-specific.html?id=${json[i].id}
+        <div class=post">
+        <img src=${json[i]._embedded["wp:featuredmedia"][0].source_url}>
+        <h2>${json[i].title.rendered}</h2>
+        <p>${json[i].excerpt.rendered}
+        </div>
+        </a>
+        `
         
     }
 }
