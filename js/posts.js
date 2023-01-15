7248bc8d649b2ee59af9616f3e5bca7519c33af2
Tonje Stensen <stensentonje@gmail.com>
2023-01-15 21:29:55 +0100

    fix

--- js/posts.js
+++ js/posts.js
@@ -1,32 +1,41 @@
+const container = document.querySelector(".posts_main-container");
+const loadMoreButton = document.querySelector(".loadMore")
 
 
 /* api call */
-const url = "https://discoverocean.tanific.one/wp-json/wp/v2/posts?_embed&per_page=10"
-const container = document.querySelector(".posts_main-container");
+const discoverOceanURL = "https://discoverocean.tanific.one/wp-json/wp/v2/posts?_embed";
+const perPage12 = `${discoverOceanURL}&per_page=12`;
 
 
-async function getPosts() {
+async function getPosts(url) {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json)
 
-    const response = await fetch(url);
-    const json = await response.json();
+        container.innerHTML = ``;
 
-    console.log(json)
-
-    for(let i = 0; i < json.length; i++) {
-
-        container.innerHTML += `
-        <a href="post-specific.html?id=${json[i].id}">
-        <div id="post">
-            <img src=${json[i]._embedded["wp:featuredmedia"][0].source_url}>
-            <h2>${json[i].title.rendered}</h2>
-            <p>${json[i].excerpt.rendered}</p>
-        </div>
-        </a>
-        `
+        for(let i = 0; i < json.length; i++) {
+            container.innerHTML += `
+            <a href="post-specific.html?id=${json[i].id}">
+            <div id="post">
+                <img src=${json[i]._embedded["wp:featuredmedia"][0].source_url}>
+                <h2>${json[i].title.rendered}</h2>
+                <p>${json[i].excerpt.rendered}</p>
+            </div>
+            </a>`
+        }
+    } catch (error) {
+        console.log(error)
     }
 }
 
-getPosts();
+getPosts(discoverOceanURL);
+
+/* view more button */
+loadMoreButton.addEventListener("click", () => {
+    getPosts(perPage12);
+});
 
 
 /* parallax scroll jellyfish */
@@ -36,3 +45,6 @@ window.addEventListener('scroll', function() {
     document.querySelector(".jelly2").style.transform = `translateY(${distance * -0.3}px)`;
     document.querySelector(".jelly3").style.transform = `translateY(${distance * -0.2}px)`;
 }) 
+
+
+
